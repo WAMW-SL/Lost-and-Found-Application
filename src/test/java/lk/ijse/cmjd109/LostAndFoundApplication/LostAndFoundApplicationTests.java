960094d6bf2937682d7ac8bf294467ebb70cb7b1
WAMAW-SL <amanthamihiran2003@gmail.com>
2025-05-17 10:41:05 +0530

    initialize repository

---
+++ src/test/java/lk/ijse/cmjd109/LostAndFoundApplication/LostAndFoundApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd109.LostAndFoundApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAndFoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
