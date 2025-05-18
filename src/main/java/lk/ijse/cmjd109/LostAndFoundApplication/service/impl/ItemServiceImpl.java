61ec8479c005a44ac71c5c61497f5138afeebd94
WAMAW-SL <amanthamihiran2003@gmail.com>
2025-05-18 23:12:34 +0530

    Create ItemServiceImpl

---
+++ src/main/java/lk/ijse/cmjd109/LostAndFoundApplication/service/impl/ItemServiceImpl.java
@@ -0,0 +1,39 @@
+package lk.ijse.cmjd109.LostAndFoundApplication.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lk.ijse.cmjd109.LostAndFoundApplication.dto.ItemDto;
+import lk.ijse.cmjd109.LostAndFoundApplication.dto.ItemStatus;
+import lk.ijse.cmjd109.LostAndFoundApplication.service.ItemService;
+
+@Service
+public class ItemServiceImpl implements ItemService{
+
+    @Override
+    public void addReport(ItemDto itemDto) {
+    
+    }
+
+    @Override
+    public void updateReport(ItemDto itemDto) {
+       
+    }
+
+    @Override
+    public void deleteReport(String reportId) {
+        
+    }
+
+    @Override
+    public ItemDto getSelectedItem(String reportId) {
+        return null;
+    }
+
+    @Override
+    public List<ItemDto> getAllItemsOfSelectedGroup(ItemStatus itemStatus) {
+       return null;
+    }
+    
+}
