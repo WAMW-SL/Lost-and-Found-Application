6e08df496e51313605aa58e566b568e5bcb47997
WAMAW-SL <amanthamihiran2003@gmail.com>
2025-05-29 15:47:30 +0530

    Create ItemEntity

---
+++ src/main/java/lk/ijse/cmjd109/LostAndFoundApplication/entities/ItemCategory.java
@@ -0,0 +1,5 @@
+package lk.ijse.cmjd109.LostAndFoundApplication.entities;
+
+public enum ItemCategory {
+    STATIONERY,KEYS,ELECTRONICS,ID_CARDS
+}
---
+++ src/main/java/lk/ijse/cmjd109/LostAndFoundApplication/entities/ItemEntity.java
@@ -0,0 +1,31 @@
+package lk.ijse.cmjd109.LostAndFoundApplication.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.cmjd109.LostAndFoundApplication.dto.ItemStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name="Item")
+public class ItemEntity {
+    @Id
+    private String reportId;
+    private String userId;
+    private String itemName;
+    private ItemCategory category;
+    private String description;
+    private String brand;
+    private String colour;
+    private LocalDate foundDate_Or_lastSeentDate;
+    private String foundLocation_Or_lastSeenLocation;
+    private String privateDetails;
+    private ItemStatus status;
+}
