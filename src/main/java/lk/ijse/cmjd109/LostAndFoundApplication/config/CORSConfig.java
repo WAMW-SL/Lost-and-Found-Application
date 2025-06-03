b7975e9f2e56eb64f8b2fdbcab949e3302fe2945
WAMAW-SL <amanthamihiran2003@gmail.com>
2025-06-04 00:13:12 +0530

    Handle CORS issue with creating CORSConfig

---
+++ src/main/java/lk/ijse/cmjd109/LostAndFoundApplication/config/CORSConfig.java
@@ -0,0 +1,25 @@
+package lk.ijse.cmjd109.LostAndFoundApplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig {
+    @Bean
+    public WebMvcConfigurer configurer(){
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET","POST","PATCH","PUT","DELETE","OPTIONS")
+                        .allowedHeaders("*");
+            }
+            
+        };
+    }
+}
