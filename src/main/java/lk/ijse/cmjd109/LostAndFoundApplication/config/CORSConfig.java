2c2004179a7a80c271ad24fc280a887eb33080ff
WAMAW-SL <amanthamihiran2003@gmail.com>
2025-06-21 19:01:22 +0530

    Modify CORSConfig

--- src/main/java/lk/ijse/cmjd109/LostAndFoundApplication/config/CORSConfig.java
+++ src/main/java/lk/ijse/cmjd109/LostAndFoundApplication/config/CORSConfig.java
@@ -1,25 +1,26 @@
 package lk.ijse.cmjd109.LostAndFoundApplication.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CORSConfig {
     @Bean
-    public WebMvcConfigurer configurer(){
-        return new WebMvcConfigurer() {
+    public CorsConfigurationSource corsConfig() {
+        var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setExposedHeaders(List.of("Authorization"));
 
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry
-                        .addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET","POST","PATCH","PUT","DELETE","OPTIONS")
-                        .allowedHeaders("*");
-            }
-            
-        };
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
